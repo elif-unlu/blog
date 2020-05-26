063033e847de1d81f8f2a1f8d972e0dde2b23e4d
Elif Hacısalihoğlu <hacisalihoglu.elif@gmail.com>
2020-05-26 19:40:41 +0300

    add nav-user

--- admin/header.php
+++ admin/header.php
@@ -5,6 +5,22 @@
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto navbar-right-top">
+                <li class="nav-item dropdown nav-user">
+                    <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/avatar-1.jpg" alt="" class="user-avatar-md rounded-circle"></a>
+                    <div class="dropdown-menu dropdown-menu-right nav-user-dropdown"
+                        aria-labelledby="navbarDropdownMenuLink2">
+                        <div class="nav-user-info">
+                            <h5 class="mb-0 text-white nav-user-name">Admin</h5>
+                        </div>
+                        <a class="dropdown-item" href="#"><i class="fas fa-user-plus mr-2"></i>Add New Admin</a>
+                        <a class="dropdown-item" href="#"><i class="fas fa-users mr-2"></i>All Admins</a>
+                        <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </nav>
 </div>
 
@@ -19,15 +35,16 @@
                 <ul class="navbar-nav flex-column">
                     <li class="nav-divider"> Menu</li>
                     <li class="nav-item ">
-                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1">
-                            <i class="fa fa-fw fa-pencil-alt"></i>Blog 
+                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false"
+                            data-target="#submenu-1" aria-controls="submenu-1">
+                            <i class="fa fa-fw fa-pencil-alt"></i>Blog
                             <span class="badge badge-success">6</span>
                         </a>
                         <div id="submenu-1" class="collapse submenu">
-                            <ul class="nav flex-column">    
+                            <ul class="nav flex-column">
                                 <li class="nav-item">
                                     <a class="nav-link" href="blog-add.php">Blog Add</a>
-                                </li>                        
+                                </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="blog-list.php">Blog List</a>
                                 </li>
